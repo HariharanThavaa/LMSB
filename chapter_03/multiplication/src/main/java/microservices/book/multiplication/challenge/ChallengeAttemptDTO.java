f475253da4e1d2febb3cb98b3e7e5726c1b5353f
Hariharan <harimaniam@gmail.com>
2021-07-01 21:49:33 +0100

    Controller and validators

--- chapter_03/multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeAttemptDTO.java
+++ chapter_03/multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeAttemptDTO.java
@@ -2,12 +2,20 @@ package microservices.book.multiplication.challenge;
 
 import lombok.Value;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
 /**
  * Attempt coming from the user.
  */
 @Value
 public class ChallengeAttemptDTO {
+    @Min(1) @Max(99)
     int factorA, factorB;
+    @NotBlank
     String userAlias;
+    @Positive(message = "negative values not allowed.")
     int guess;
 }
